18625e0c24b445d066b6188483cdd68d5b3f40d2
Sasha <a.vicente.grab@gmail.com>
2015-06-08 02:03:08 +0300

    Simplify preprocess script

---
+++ preprocess.R
@@ -0,0 +1,14 @@
+# Get important libraries
+library(ggplot2)     # Grammar of Graphics
+library(gcookbook)
+library(gdata)
+
+# Run data analysis step by step
+source('SurveyQuestions.R')
+
+source('ReadData.R')
+
+source('BasicClean.R')
+
+source('SimpleStats.R')
+
